4c759317e5b38c501421429325346cd3688576e4
Thomas Sødring <tsodring@gmail.com>
2018-11-24 08:31:21 +0100

    Minor updates for OData
    Make sure the address is correct. Also remove reference to angular min.

--- web/dependencies/internal/search.js
+++ web/dependencies/internal/search.js
@@ -14,7 +14,7 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
 	console.log(" The value of element is " + elementValue);
 	console.log(" The value of filter is " + filterValue);
 	console.log(" The value of string is " + stringValue);
-      var urlToFonds = baseUrl + "arkivstruktur/" + elementValue + "?%24filter%3Dcontains%28" + contentValue + "%2C%27" + stringValue + "%27%29%24top%3D2%24skip%3D0%24orderby%3D" + contentValue + "%20desc";
+      var urlToFonds = baseUrl + "odata/arkivstruktur/" + elementValue + "?%24filter%3Dcontains%28" + contentValue + "%2C%27" + stringValue + "%27%29%24top%3D2%24skip%3D0%24orderby%3D" + contentValue + "%20desc";
 	console.log("Attempting search on " + urlToFonds);
 	var token = GetUserToken();
 	$http({
@@ -24,7 +24,7 @@ app.controller('SearchController', ['$scope', '$http', function ($scope, $http) 
 	}).then(function successCallback(response) {
 	    console.log("Data received from search is : " + JSON.stringify(response.data));
 	}, function errorCallback(response) {
-	    console.log("Error when attempting search. Response is " + response);
+    console.log("Error when attempting search. Response is " + JSON.stringify(response));
 	});
     }
 }]);
