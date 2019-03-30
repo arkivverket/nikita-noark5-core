fead9385d06071a6beae5a865fc7820376a69710
Thomas Sødring <tsodring@gmail.com>
2019-03-30 21:47:23 +0100

    Remove non-used code
    These are left over from testing different approaches. Coverity is
    reporting bugs in them so removing them.

--- src/main/java/nikita/webapp/web/controller/application/saml/CurrentUser.java
+++
@@ -1,9 +0,0 @@
-package nikita.webapp.web.controller.application.saml;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface CurrentUser {
-}
