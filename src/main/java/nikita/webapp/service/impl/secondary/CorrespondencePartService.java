3e913f096b5c2470b717d2ea40ec76a3479d454e
Thomas Sødring <tsodring@gmail.com>
2019-03-30 23:47:32 +0100

    Renable disabled code
    Ths code to update was disabled while we were having problems figuring
    out the domain model. This should be working now.

--- src/main/java/nikita/webapp/service/impl/secondary/CorrespondencePartService.java
+++ src/main/java/nikita/webapp/service/impl/secondary/CorrespondencePartService.java
@@ -1,9 +1,6 @@
 package nikita.webapp.service.impl.secondary;
 
-import nikita.common.model.noark5.v4.casehandling.secondary.CorrespondencePart;
-import nikita.common.model.noark5.v4.casehandling.secondary.CorrespondencePartInternal;
-import nikita.common.model.noark5.v4.casehandling.secondary.CorrespondencePartPerson;
-import nikita.common.model.noark5.v4.casehandling.secondary.CorrespondencePartUnit;
+import nikita.common.model.noark5.v4.casehandling.secondary.*;
 import nikita.common.repository.n5v4.secondary.ICorrespondencePartRepository;
 import nikita.common.util.exceptions.NoarkEntityNotFoundException;
 import nikita.webapp.service.interfaces.secondary.ICorrespondencePartService;
@@ -20,11 +17,8 @@ import static nikita.common.config.Constants.INFO_CANNOT_FIND_OBJECT;
 @Transactional
 public class CorrespondencePartService implements ICorrespondencePartService {
 
-    private static final Logger logger = LoggerFactory.getLogger(CorrespondencePartService.class);
-
-    // TODO: Trying to pick up property from yaml file, but not working ...
-    //@Value("${nikita-noark5-core.pagination.maxPageSize}")
-    Integer maxPageSize = new Integer(10);
+    private static final Logger logger =
+            LoggerFactory.getLogger(CorrespondencePartService.class);
 
     private ICorrespondencePartRepository correspondencePartRepository;
 
@@ -32,26 +26,50 @@ public class CorrespondencePartService implements ICorrespondencePartService {
         this.correspondencePartRepository = correspondencePartRepository;
     }
 
+    /**
+     * Update the CorrespondencePartPerson identified by systemId. Retrieve a
+     * copy of the CorrespondencePartPerson from the database. If it does not
+     * exist throw a not found exception. If it exists, call sub methods that
+     * copy the values. Save the updated object back to the database.
+     *
+     * @param systemId                   systemId of the CorrespondencePartPerson to update
+     * @param version                    ETAG version
+     * @param incomingCorrespondencePart incoming CorrespondencePartPerson with
+     *                                   values to copy from
+     * @return the updated CorrespondencePartPerson
+     */
     @Override
-    public CorrespondencePartPerson updateCorrespondencePartPerson(String systemId, Long version,
-                                                                   CorrespondencePartPerson incomingCorrespondencePart) {
-        /*
-        TODO: Temp disabled!
+    public CorrespondencePartPerson
+    updateCorrespondencePartPerson(String systemId, Long version,
+                                   CorrespondencePartPerson
+                                           incomingCorrespondencePart) {
         CorrespondencePartPerson existingCorrespondencePart =
-                (CorrespondencePartPerson) getCorrespondencePartOrThrow(systemId);
+                (CorrespondencePartPerson)
+                        getCorrespondencePartOrThrow(systemId);
 
         // Copy all the values you are allowed to copy ....
-        existingCorrespondencePart.setContactInformation(incomingCorrespondencePart.getContactInformation());
-        existingCorrespondencePart.setdNumber(incomingCorrespondencePart.getdNumber());
-        existingCorrespondencePart.setName(incomingCorrespondencePart.getName());
-        existingCorrespondencePart.setSocialSecurityNumber(incomingCorrespondencePart.getSocialSecurityNumber());
-        existingCorrespondencePart.setPostalAddress(incomingCorrespondencePart.getPostalAddress());
-        existingCorrespondencePart.setResidingAddress(incomingCorrespondencePart.getResidingAddress());
+        // First the values
+        existingCorrespondencePart.setdNumber(
+                incomingCorrespondencePart.getdNumber());
+        existingCorrespondencePart.setName(
+                incomingCorrespondencePart.getName());
+        existingCorrespondencePart.setSocialSecurityNumber(
+                incomingCorrespondencePart.getSocialSecurityNumber());
+
+        // Then secondary objects
+        updateContactInformation(existingCorrespondencePart,
+                incomingCorrespondencePart);
+        updateResidingAddress(existingCorrespondencePart,
+                incomingCorrespondencePart);
+        updatePostalAddress(existingCorrespondencePart,
+                incomingCorrespondencePart);
+        updateResidingAddress(existingCorrespondencePart,
+                incomingCorrespondencePart);
+
+        // Check the ETAG
         existingCorrespondencePart.setVersion(version);
         correspondencePartRepository.save(existingCorrespondencePart);
-        return existingCorrespondencePart;*/
-        return null;
-
+        return existingCorrespondencePart;
     }
 
     @Override
@@ -83,8 +101,8 @@ public class CorrespondencePartService implements ICorrespondencePartService {
 
         existingCorrespondencePart.setPostalAddress(incomingCorrespondencePart.getPostalAddress());
         */
-        //existingCorrespondencePart.setContactInformation
-        // (incomingCorrespondencePart.getContactInformation());
+        //existingCorrespondencePart.setResidingAddress
+        // (incomingCorrespondencePart.getResidingAddress());
         existingCorrespondencePart.setContactPerson(incomingCorrespondencePart.getContactPerson());
         existingCorrespondencePart.setOrganisationNumber(incomingCorrespondencePart.getOrganisationNumber());
         existingCorrespondencePart.setName(incomingCorrespondencePart.getName());
@@ -164,5 +182,78 @@ public class CorrespondencePartService implements ICorrespondencePartService {
         entityManager.flush();
         entityManager.clear();*/
         correspondencePartRepository.delete(correspondencePartInternal);
+    }
+
+    // Internal helper methods
+
+    public ContactInformation updateContactInformation(
+            CorrespondencePartPerson existingCorrespondencePart,
+            CorrespondencePartPerson incomingCorrespondencePart) {
+
+        ContactInformation updatedContactInformation =
+                incomingCorrespondencePart.getContactInformation();
+        ContactInformation contactInformation =
+                existingCorrespondencePart.getContactInformation();
+
+        contactInformation.setEmailAddress(
+                updatedContactInformation.getEmailAddress());
+        contactInformation.setMobileTelephoneNumber(
+                updatedContactInformation.getMobileTelephoneNumber());
+        contactInformation.setTelephoneNumber(
+                updatedContactInformation.getTelephoneNumber());
+
+        return contactInformation;
+    }
+
+    /**
+     * Update PostalAddress. Copy all values from the incoming payload to a
+     * retrieved copy from the database. Actual copying is done by calling
+     * copyAddress.
+     *
+     * @param existingCorrespondencePartPerson from the database
+     * @param incomingCorrespondencePartPerson incoming payload from a client
+     */
+    public void updatePostalAddress(
+            CorrespondencePartPerson existingCorrespondencePartPerson,
+            CorrespondencePartPerson incomingCorrespondencePartPerson) {
+        copyAddress(existingCorrespondencePartPerson.getPostalAddress().
+                        getSimpleAddress(),
+                incomingCorrespondencePartPerson.getPostalAddress().
+                        getSimpleAddress());
+    }
+
+    /**
+     * Update ResidingAddress. Copy all values from the incoming payload to a
+     * retrieved copy from the database. Actual copying is done by calling
+     * copyAddress.
+     *
+     * @param existingCorrespondencePartPerson from the database
+     * @param incomingCorrespondencePartPerson incoming payload from a client
+     */
+    public void updateResidingAddress(
+            CorrespondencePartPerson existingCorrespondencePartPerson,
+            CorrespondencePartPerson incomingCorrespondencePartPerson) {
+        copyAddress(existingCorrespondencePartPerson.
+                        getResidingAddress().getSimpleAddress(),
+                incomingCorrespondencePartPerson.
+                        getResidingAddress().getSimpleAddress());
+    }
+
+    /**
+     * Copy the values you are allowed to copy from the incoming address object
+     * to the existing address object in the database.
+     *
+     * @param existingAddress An existing address object
+     * @param incomingAddress Incoming address object
+     */
+    private void copyAddress(SimpleAddress existingAddress,
+                             SimpleAddress incomingAddress) {
+        existingAddress.setAddressType(incomingAddress.getAddressType());
+        existingAddress.setAddressLine1(incomingAddress.getAddressLine1());
+        existingAddress.setAddressLine2(incomingAddress.getAddressLine2());
+        existingAddress.setAddressLine3(incomingAddress.getAddressLine3());
+        existingAddress.setPostalNumber(incomingAddress.getPostalNumber());
+        existingAddress.setPostalTown(incomingAddress.getPostalTown());
+        existingAddress.setCountryCode(incomingAddress.getCountryCode());
     }
 }
