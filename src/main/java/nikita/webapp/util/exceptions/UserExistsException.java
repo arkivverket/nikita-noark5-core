1c2b2b806392337c4f7abc5f84e5d1494590e72e
Thomas Sødring <tsodring@gmail.com>
2019-05-04 08:28:57 +0200

    Remove unused class
    Reported by FindBugs

--- src/main/java/nikita/webapp/util/exceptions/UserExistsException.java
+++
@@ -1,9 +0,0 @@
-package nikita.webapp.util.exceptions;
-
-public class UserExistsException extends Throwable {
-
-    public UserExistsException(final String message) {
-        super(message);
-    }
-
-}
