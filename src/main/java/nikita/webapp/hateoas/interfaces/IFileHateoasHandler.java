f2ff958c7c745061ebb41438fada270560c78e32
Thomas Sødring <tsodring@gmail.com>
2019-05-30 23:50:06 +0200

    Tidy formatting

--- src/main/java/nikita/webapp/hateoas/interfaces/IFileHateoasHandler.java
+++ src/main/java/nikita/webapp/hateoas/interfaces/IFileHateoasHandler.java
@@ -11,43 +11,61 @@ import nikita.common.model.noark5.v5.interfaces.entities.INikitaEntity;
 public interface IFileHateoasHandler
         extends IHateoasHandler {
 
-
     void addSeries(INikitaEntity entity,
                    IHateoasNoarkObject hateoasNoarkObject);
 
-    void addEndFile(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addEndFile(INikitaEntity entity,
+                    IHateoasNoarkObject hateoasNoarkObject);
 
-    void addExpandToCaseFile(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addExpandToCaseFile(INikitaEntity entity,
+                             IHateoasNoarkObject hateoasNoarkObject);
 
-    void addExpandToMeetingFile(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addExpandToMeetingFile(INikitaEntity entity,
+                                IHateoasNoarkObject hateoasNoarkObject);
 
-    void addRecord(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addRecord(INikitaEntity entity,
+                   IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewRecord(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewRecord(INikitaEntity entity,
+                      IHateoasNoarkObject hateoasNoarkObject);
 
-    void addComment(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addComment(INikitaEntity entity,
+                    IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewComment(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewComment(INikitaEntity entity,
+                       IHateoasNoarkObject hateoasNoarkObject);
 
-    void addSubFile(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addSubFile(INikitaEntity entity,
+                    IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewSubFile(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewSubFile(INikitaEntity entity,
+                       IHateoasNoarkObject hateoasNoarkObject);
 
-    void addCrossReference(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addCrossReference(INikitaEntity entity,
+                           IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewCrossReference(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewCrossReference(INikitaEntity entity,
+                              IHateoasNoarkObject hateoasNoarkObject);
 
-    void addClass(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addClass(INikitaEntity entity,
+                  IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewClass(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewClass(INikitaEntity entity,
+                     IHateoasNoarkObject hateoasNoarkObject);
 
-    void addReferenceSeries(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addReferenceSeries(INikitaEntity entity,
+                            IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewReferenceSeries(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewReferenceSeries(INikitaEntity entity,
+                               IHateoasNoarkObject hateoasNoarkObject);
 
-    void addReferenceSecondaryClassification(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addReferenceSecondaryClassification(
+            INikitaEntity entity,
+            IHateoasNoarkObject hateoasNoarkObject);
 
-    void addNewReferenceSecondaryClassification(INikitaEntity entity, IHateoasNoarkObject hateoasNoarkObject);
+    void addNewReferenceSecondaryClassification(
+            INikitaEntity entity,
+            IHateoasNoarkObject hateoasNoarkObject);
 
     void addParty(INikitaEntity entity,
                   IHateoasNoarkObject hateoasNoarkObject);
