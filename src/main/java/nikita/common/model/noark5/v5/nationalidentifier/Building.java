4f6cb704d36a436b8c3b38fd00a5faec0ffa58bf
Thomas Sødring <tsodring@gmail.com>
2019-07-04 14:32:05 +0200

    Introduce Building as a National Identifier class. This is as described in the interface standard.

---
+++ src/main/java/nikita/common/model/noark5/v5/nationalidentifier/Building.java
@@ -0,0 +1,72 @@
+package nikita.common.model.noark5.v5.nationalidentifier;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.Table;
+
+import static javax.persistence.InheritanceType.JOINED;
+import static nikita.common.config.Constants.TABLE_BUILDING;
+
+@Entity
+@Table(name = TABLE_BUILDING)
+@Inheritance(strategy = JOINED)
+public class Building
+        extends NationalIdentifier {
+
+    /**
+     * M??? bygningsnummer - (xs:integer)
+     */
+    @Column(name = "building_number", nullable = false)
+    @Audited
+    Integer buildingNumber;
+
+    /**
+     * M??? endringsloepenummer - (xs:integer)
+     */
+    @Column(name = "building_change_number")
+    @Audited
+    Integer continuousNumberingOfBuildingChange;
+
+    @Override
+    public String toString() {
+        return "Building{" +
+                "buildingNumber=" + buildingNumber +
+                ", continuousNumberingOfBuildingChange=" +
+                continuousNumberingOfBuildingChange +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        Building rhs = (Building) other;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(other))
+                .append(buildingNumber, rhs.buildingNumber)
+                .append(continuousNumberingOfBuildingChange,
+                        rhs.continuousNumberingOfBuildingChange)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(buildingNumber)
+                .append(continuousNumberingOfBuildingChange)
+                .toHashCode();
+    }
+}
