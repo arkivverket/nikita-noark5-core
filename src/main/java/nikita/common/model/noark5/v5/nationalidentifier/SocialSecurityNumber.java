9fcddae3c43d26c1c7613d6b21792e4e1acfc318
Thomas Sødring <tsodring@gmail.com>
2019-07-04 14:01:36 +0200

    Introduce SocialSecurityNumber as a base class

---
+++ src/main/java/nikita/common/model/noark5/v5/nationalidentifier/SocialSecurityNumber.java
@@ -0,0 +1,67 @@
+package nikita.common.model.noark5.v5.nationalidentifier;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.Table;
+
+import static javax.persistence.InheritanceType.JOINED;
+import static nikita.common.config.Constants.TABLE_SOCIAL_SECURITY_NUMBER;
+
+@Entity
+@Table(name = TABLE_SOCIAL_SECURITY_NUMBER)
+@Inheritance(strategy = JOINED)
+public class SocialSecurityNumber
+        extends PersonIdentifier {
+    /**
+     * M??? - fødselsnummer (xs:string)
+     */
+    @Column(name = "social_security_number")
+    @Audited
+    private String socialSecurityNumber;
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "SocialSecurityNumber{" +
+                "socialSecurityNumber='" + socialSecurityNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        if (other == this) {
+            return true;
+        }
+        if (other.getClass() != getClass()) {
+            return false;
+        }
+        SocialSecurityNumber rhs = (SocialSecurityNumber) other;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(other))
+                .append(socialSecurityNumber, rhs.socialSecurityNumber)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(socialSecurityNumber)
+                .toHashCode();
+    }
+}
