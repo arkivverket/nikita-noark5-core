a2b41cb210e4c6faacf5d9011cdce273fd59066a
Thomas Sødring <tsodring@gmail.com>
2019-07-10 21:51:14 +0200

    Fix casting problem
    Need to pick out the username

--- src/main/java/nikita/webapp/spring/AuditorAwareImpl.java
+++ src/main/java/nikita/webapp/spring/AuditorAwareImpl.java
@@ -1,15 +1,11 @@
 package nikita.webapp.spring;
 
 import nikita.common.model.nikita.NikitaUserPrincipal;
-import nikita.common.model.noark5.v5.admin.User;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.annotation.ManagedBean;
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 import java.util.Optional;
 
 @ManagedBean
@@ -29,6 +25,8 @@ public class AuditorAwareImpl
         if (authentication == null || !authentication.isAuthenticated()) {
             return Optional.of("system");
         }
-        return Optional.of((String)authentication.getPrincipal());
+        NikitaUserPrincipal principal =
+                (NikitaUserPrincipal) authentication.getPrincipal();
+        return Optional.of(principal.getUsername());
     }
 }
