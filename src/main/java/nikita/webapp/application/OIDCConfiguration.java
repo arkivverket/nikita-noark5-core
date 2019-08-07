e08b48fff510224cb26dec812921c01d2aba4d0e
tsodring <_Simeyhh17>
2019-08-07 18:47:03 +0200

    Remove redundant JsonProperty
    Not useful when we take care of serialising ourselves.

--- src/main/java/nikita/webapp/application/OIDCConfiguration.java
+++ src/main/java/nikita/webapp/application/OIDCConfiguration.java
@@ -1,78 +1,38 @@
 package nikita.webapp.application;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import nikita.webapp.hateoas.application.OIDCConfigurationSerializer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @JsonSerialize(using = OIDCConfigurationSerializer.class)
 public class OIDCConfiguration {
 
-    @JsonProperty("issuer")
     private String issuer;
-
-    @JsonProperty("authorization_endpoint")
     private String authorizationEndpoint;
-
-    @JsonProperty("token_endpoint")
     private String tokenEndpoint;
-
-    @JsonProperty("userinfo_endpoint")
     private String userinfoEndpoint;
-
-    @JsonProperty("registration_endpoint")
     private String registrationEndpoint;
-
-    @JsonProperty("jwks_uri")
     private String jwksUri;
-
-    @JsonProperty("response_types_supported")
-    private List<String> responseTypesSupported = new ArrayList();
-
-    @JsonProperty("response_modes_supported")
-    private List<String> responseModesSupported = new ArrayList();
-
-    @JsonProperty("grant_types_supported")
-    private List<String> grantTypesSupported = new ArrayList();
-
-    @JsonProperty("subject_types_supported")
-    private List<String> subjectTypesSupported = new ArrayList();
-
-    @JsonProperty("id_token_signing_alg_values_supported")
-    private List<String> idTokenSigningAlgValuesSupported = new ArrayList();
-
-    @JsonProperty("scopes_supported")
-    private List<String> scopesSupported = new ArrayList();
-
-    @JsonProperty("token_endpoint_auth_methods_supported")
-    private List<String> tokenEndpointAuthMethodsSupported = new ArrayList();
-
-    @JsonProperty("claims_supported")
-    private List<String> claimsSupported = new ArrayList();
-
-    @JsonProperty("introspection_endpoint")
+    private List<String> responseTypesSupported = new ArrayList<>();
+    private List<String> responseModesSupported = new ArrayList<>();
+    private List<String> grantTypesSupported = new ArrayList<>();
+    private List<String> subjectTypesSupported = new ArrayList<>();
+    private List<String> idTokenSigningAlgValuesSupported = new ArrayList<>();
+    private List<String> scopesSupported = new ArrayList<>();
+    private List<String> tokenEndpointAuthMethodsSupported = new ArrayList<>();
+    private List<String> claimsSupported = new ArrayList<>();
     private String introspectionEndpoint;
-
-    @JsonProperty("introspection_endpoint_auth_methods_supported")
-    private List<String> introspectionEndpointAuthMethodsSupported = new ArrayList();
-
-    @JsonProperty("revocation_endpoint")
+    private List<String> introspectionEndpointAuthMethodsSupported =
+            new ArrayList<>();
     private String revocationEndpoint;
-
-    @JsonProperty("revocation_endpoint_auth_methods_supported")
-    private List<String> revocationEndpointAuthMethodsSupported = new ArrayList();
-
-    @JsonProperty("end_session_endpoint")
+    private List<String> revocationEndpointAuthMethodsSupported =
+            new ArrayList<>();
     private String endSessionEndpoint;
-
-    @JsonProperty("request_parameter_supported")
     private Boolean requestParameterSupported;
-
-    @JsonProperty("request_object_signing_alg_values_supported")
-    private List<String> requestObjectSigningAlgValuesSupported = new ArrayList();
+    private List<String> requestObjectSigningAlgValuesSupported =
+            new ArrayList<>();
 
     public String getIssuer() {
         return issuer;
