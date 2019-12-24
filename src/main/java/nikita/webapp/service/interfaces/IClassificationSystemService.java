31f91d3a12bc30f307a711fcf6c44812438afa3c
Petter Reinholdtsen <pere@hungry.com>
2019-12-24 19:13:54 +0100

    Implement GET for .../arkivdel/<id>/ny-klassifikasjonssystem/

--- src/main/java/nikita/webapp/service/interfaces/IClassificationSystemService.java
+++ src/main/java/nikita/webapp/service/interfaces/IClassificationSystemService.java
@@ -44,4 +44,6 @@ public interface IClassificationSystemService {
     void deleteEntity(@NotNull String classificationSystemSystemId);
 
     long deleteAllByOwnedBy();
+
+    ClassificationSystemHateoas generateDefaultClassificationSystem();
 }
