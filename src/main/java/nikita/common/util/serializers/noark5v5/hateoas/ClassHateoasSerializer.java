d4de1749981ad334fa9fd8e80006f1ddcef96776
Petter Reinholdtsen <pere@hungry.com>
2020-01-01 14:44:08 +0100

    Use full name to CLASS_ID define.

--- src/main/java/nikita/common/util/serializers/noark5v5/hateoas/ClassHateoasSerializer.java
+++ src/main/java/nikita/common/util/serializers/noark5v5/hateoas/ClassHateoasSerializer.java
@@ -34,7 +34,7 @@ public class ClassHateoasSerializer extends HateoasSerializer implements IHateoa
         jgen.writeStartObject();
         CommonUtils.Hateoas.Serialize.printSystemIdEntity(jgen, klass);
         if (klass.getClassId() != null) {
-            jgen.writeStringField(CLASS_ID, klass.getClassId());
+            jgen.writeStringField(N5ResourceMappings.CLASS_ID, klass.getClassId());
         }
         CommonUtils.Hateoas.Serialize.printTitleAndDescription(jgen, klass);
         CommonUtils.Hateoas.Serialize.printCreateEntity(jgen, klass);
