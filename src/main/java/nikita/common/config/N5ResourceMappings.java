arkivverket/nikita-noark5-core

commit 03c6582f182620e19962db9eb91db7d367dff89f

Correct case of PART_ROLE.

src/main/java/nikita/common/config/N5ResourceMappings.java
@@ -109,7 +109,7 @@ public final class N5ResourceMappings {
     public static final String PART_UNIT = "partenhet";
     public static final String PART_ID = "partID";
     public static final String PART_NAME = "partNavn";
-    public static final String PART_ROLE = "partrolle";
+    public static final String PART_ROLE = "partRolle";
 
     // Record
     public static final String RECORD_ARCHIVED_BY = "arkivertAv";

commit b4220dac3b17c625fddbad80927c5f8bf2d920d0

Handle both 'partrolle' and 'partRolle' parts.
The former is the metadata entity and relation key part, the second
is the field name in part.

This fixes build problem introduced in
03c6582f182620e19962db9eb91db7d367dff89f.

src/main/java/nikita/common/config/N5ResourceMappings.java
@@ -104,12 +104,13 @@ public final class N5ResourceMappings {
     public static final String CASE_LOANED_TO = "utlaantTil";
 
     // Part
+    public static final String PART_ROLE = "partrolle";
     public static final String PART = "part";
     public static final String PART_PERSON = "partperson";
     public static final String PART_UNIT = "partenhet";
     public static final String PART_ID = "partID";
     public static final String PART_NAME = "partNavn";
-    public static final String PART_ROLE = "partRolle";
+    public static final String PART_ROLE_FIELD = "partRolle";
 
     // Record
     public static final String RECORD_ARCHIVED_BY = "arkivertAv";
@@ -414,7 +415,7 @@ public final class N5ResourceMappings {
     public static final String PART_ENG = "part";
     public static final String PART_ID_ENG = "partID";
     public static final String PART_NAME_ENG = "partNavn";
-    public static final String PART_ROLE_ENG = "partrolle";
+    public static final String PART_ROLE_FIELD_ENG = "partrolle";
     public static final String CASE_RECORDS_MANAGEMENT_UNIT_ENG = "journalenhet";
     public static final String CASE_RESPONSIBLE_ENG = "saksansvarlig";
     public static final String CASE_SEQUENCE_NUMBER_ENG = "sakssekvensnummer";
@@ -626,7 +627,7 @@ public final class N5ResourceMappings {
     public static final String PART_ENG_OBJECT = "part";
     public static final String PART_ID_ENG_OBJECT = "partID";
     public static final String PART_NAME_ENG_OBJECT = "partNavn";
-    public static final String PART_ROLE_ENG_OBJECT = "partrolle";
+    public static final String PART_ROLE_FIELD_ENG_OBJECT = "partrolle";
     public static final String CASE_RECORDS_MANAGEMENT_UNIT_ENG_OBJECT = "journalenhet";
     public static final String CASE_RESPONSIBLE_ENG_OBJECT = "saksansvarlig";
     public static final String CASE_SEQUENCE_NUMBER_ENG_OBJECT = "sakssekvensnummer";

src/main/java/nikita/common/util/CommonUtils.java
@@ -572,7 +572,7 @@ public final class CommonUtils {
                     ObjectNode objectNode, StringBuilder errors) {
                 // Deserialize parttype
                 JsonNode currentNode =
-                        objectNode.get(PART_ROLE);
+                        objectNode.get(PART_ROLE_FIELD);
                 if (null != currentNode) {
 
                     JsonNode node = currentNode.get(CODE);
@@ -584,12 +584,12 @@ public final class CommonUtils {
                         part.setPartTypeCodeName(node.textValue());
                     }
                     if (null != part.getPartTypeCode()) {
-                        objectNode.remove(PART_ROLE);
+                        objectNode.remove(PART_ROLE_FIELD);
                     }
                 } else {
                     errors.append("The Part object you tried to");
                     errors.append(" create is missing  ");
-                    errors.append(PART_ROLE);
+                    errors.append(PART_ROLE_FIELD);
                 }
             }
 
@@ -2024,7 +2024,7 @@ public final class CommonUtils {
                 if (part != null) {
                     printSystemIdEntity(jgen, part);
                     if (part.getPartTypeCode() != null) {
-                        jgen.writeObjectFieldStart(PART_ROLE);
+                        jgen.writeObjectFieldStart(PART_ROLE_FIELD);
                         printCode(jgen, part.getPartTypeCode(),
                                 part.getPartTypeCodeName());
                         jgen.writeEndObject();

src/main/java/nikita/webapp/util/InternalNameTranslator.java
@@ -240,9 +240,9 @@ public class InternalNameTranslator {
                         PART_NAME_ENG,
                         PART_NAME_ENG_OBJECT);
         addNorToEnglishNameMap
-                (PART_ROLE,
-                        PART_ROLE_ENG,
-                        PART_ROLE_ENG_OBJECT);
+                (PART_ROLE_FIELD,
+                        PART_ROLE_FIELD_ENG,
+                        PART_ROLE_FIELD_ENG_OBJECT);
         addNorToEnglishNameMap
                 (CASE_RECORDS_MANAGEMENT_UNIT,
                         CASE_RECORDS_MANAGEMENT_UNIT_ENG,