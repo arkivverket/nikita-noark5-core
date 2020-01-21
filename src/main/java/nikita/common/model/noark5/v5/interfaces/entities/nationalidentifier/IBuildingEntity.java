6bf8399c2f7db4fcbfc1643513ffac401017a16b
Thomas Sødring <tsodring@gmail.com>
2020-01-21 12:34:05 +0100

    Remove redundant extends and use of comparable
    ISystemId extends INoarkEntity

--- src/main/java/nikita/common/model/noark5/v5/interfaces/entities/nationalidentifier/IBuildingEntity.java
+++ src/main/java/nikita/common/model/noark5/v5/interfaces/entities/nationalidentifier/IBuildingEntity.java
@@ -1,12 +1,9 @@
 package nikita.common.model.noark5.v5.interfaces.entities.nationalidentifier;
 
-import nikita.common.model.noark5.v5.NoarkEntity;
-import nikita.common.model.noark5.v5.SystemIdEntity;
-import nikita.common.model.noark5.v5.interfaces.entities.INoarkEntity;
 import nikita.common.model.noark5.v5.interfaces.entities.ISystemId;
 
 public interface IBuildingEntity
-    extends INoarkEntity, ISystemId, Comparable<SystemIdEntity> {
+        extends ISystemId {
     Integer getBuildingNumber();
 
     void setBuildingNumber(Integer buildingNumber);
--- src/main/java/nikita/common/model/noark5/v5/interfaces/entities/nationalidentifier/IPositionEntity.java
+++ src/main/java/nikita/common/model/noark5/v5/interfaces/entities/nationalidentifier/IPositionEntity.java
@@ -1,12 +1,9 @@
 package nikita.common.model.noark5.v5.interfaces.entities.nationalidentifier;
 
-import nikita.common.model.noark5.v5.NoarkEntity;
-import nikita.common.model.noark5.v5.SystemIdEntity;
-import nikita.common.model.noark5.v5.interfaces.entities.INoarkEntity;
 import nikita.common.model.noark5.v5.interfaces.entities.ISystemId;
 
 public interface IPositionEntity
-        extends INoarkEntity, ISystemId, Comparable<SystemIdEntity> {
+        extends ISystemId {
     String getCoordinateSystemCode();
 
     void setCoordinateSystemCode(String coordinateSystemCode);
