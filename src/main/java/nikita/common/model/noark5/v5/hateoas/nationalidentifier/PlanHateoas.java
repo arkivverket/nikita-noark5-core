6261c82913faf69afb97c02c98f802fd667631f2
Thomas Sødring <tsodring@gmail.com>
2020-01-24 08:09:50 +0100

    Assign correct serialiser
    The wrong serialiser was assigned to PanHateaoas

--- src/main/java/nikita/common/model/noark5/v5/hateoas/nationalidentifier/PlanHateoas.java
+++ src/main/java/nikita/common/model/noark5/v5/hateoas/nationalidentifier/PlanHateoas.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import nikita.common.model.noark5.v5.hateoas.HateoasNoarkObject;
 import nikita.common.model.noark5.v5.hateoas.IHateoasNoarkObject;
 import nikita.common.model.noark5.v5.interfaces.entities.INoarkEntity;
-import nikita.common.util.serializers.noark5v5.hateoas.nationalidentifier.DNumberSerializer;
+import nikita.common.util.serializers.noark5v5.hateoas.nationalidentifier.PlanSerializer;
 
 import java.util.List;
 
 import static nikita.common.config.N5ResourceMappings.PLAN;
 
-@JsonSerialize(using = DNumberSerializer.class)
+@JsonSerialize(using = PlanSerializer.class)
 public class PlanHateoas
         extends HateoasNoarkObject
         implements IHateoasNoarkObject {
