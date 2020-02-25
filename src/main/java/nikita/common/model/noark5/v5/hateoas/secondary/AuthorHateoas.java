97c7af6063b552eab09dacc6db1b92b53e4cdc53
Petter Reinholdtsen <pere@hungry.com>
2020-02-25 18:37:28 +0100

    Drop useless @JsonDeserialize annotation from Hateoas classes.
    Deserialising is always done for the model instances, not the
    *Hateoas classes.

--- src/main/java/nikita/common/model/noark5/v5/hateoas/secondary/AuthorHateoas.java
+++ src/main/java/nikita/common/model/noark5/v5/hateoas/secondary/AuthorHateoas.java
@@ -1,17 +1,14 @@
 package nikita.common.model.noark5.v5.hateoas.secondary;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import nikita.common.model.noark5.v5.hateoas.HateoasNoarkObject;
 import nikita.common.model.noark5.v5.hateoas.IHateoasNoarkObject;
 import nikita.common.model.noark5.v5.interfaces.entities.INoarkEntity;
-import nikita.common.util.deserialisers.secondary.AuthorDeserializer;
 import nikita.common.util.serializers.noark5v5.hateoas.secondary.AuthorHateoasSerializer;
 
 import java.util.List;
 
 @JsonSerialize(using = AuthorHateoasSerializer.class)
-@JsonDeserialize(using = AuthorDeserializer.class)
 public class AuthorHateoas
         extends HateoasNoarkObject
         implements IHateoasNoarkObject {
