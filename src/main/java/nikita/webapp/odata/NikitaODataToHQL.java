a51c761f09cb3319054c247591c059b96c1f7b8b
Thomas Sødring <tsodring@gmail.com>
2020-05-13 11:09:01 +0200

    Introduce NikitaODataToHQL Used to handle special adaptions to ODataToHQL so that particular interpretations e.g. complexTypes that are not database tables are dealt with

---
+++ src/main/java/nikita/webapp/odata/NikitaODataToHQL.java
@@ -0,0 +1,7 @@
+package nikita.webapp.odata;
+
+// Class to handle special adaptions to ODataToHQL so that
+// particular interpretations e.g. complexTypes that are not
+// database tables are dealt with
+public class NikitaODataToHQL {
+}
