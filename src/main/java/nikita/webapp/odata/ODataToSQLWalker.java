92016d5587a0715d308b685eb5a7df9f35d76c28
Thomas Sødring <tsodring@gmail.com>
2020-05-13 11:05:49 +0200

    Rename classes
    Refactoring work to make a cleaner separation of concerns
    and clearer approach. These are not under active development
    but are here to show how OData can be converted using our
    approach.

--- src/main/java/nikita/webapp/odata/ODataToSQLWalker.java
+++ src/main/java/nikita/webapp/odata/ODataToSQLWalker.java
@@ -13,7 +13,7 @@ import static nikita.common.config.ODataConstants.*;
  * command to SQL.
  */
 
-public class NikitaODataToSQLWalker {
+public class ODataToSQLWalker {
     // There is not time right now to get this code also in place. The 
     // concept is definitely nice, to convert OData to HQL, SQL and ES, but HQL
     // is all we require at the moment. Leaving it in the repo commented out
@@ -21,12 +21,12 @@ public class NikitaODataToSQLWalker {
     // now and again
 
     private static final Logger logger =
-            LoggerFactory.getLogger(NikitaODataToSQLWalker.class);
+            LoggerFactory.getLogger(ODataToSQLWalker.class);
 
     private SQLStatementBuilder sqlStatementBuilder;
     private Map<String, String> comparatorMap;
 
-    public NikitaODataToSQLWalker() {
+    public ODataToSQLWalker() {
         comparatorMap = new HashMap<>();
         comparatorMap.put(ODATA_EQ, SQL_EQ);
         comparatorMap.put(ODATA_GT, SQL_GT);
