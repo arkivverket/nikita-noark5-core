708d1792db6e01dd382f3dc70e5f9db391002014
Thomas Sødring <tsodring@gmail.com>
2020-06-22 15:54:54 +0200

    Make Odata search work in 'demo' mode
    Make it possible to run OData queries without logging in

--- web/dependencies/internal/search.js
+++ web/dependencies/internal/search.js
@@ -1,220 +1,110 @@
-var app = angular.module('nikita-search', []);
-
 /**
- *
+ * This is currently only intended to be used by nikita.oslomet.no. That is
+ * the reason for hard-coded values.
  */
-app.directive('loginModal', function () {
-  return {
-    restrict: 'A',
-    link: function (scope, element) {
-      scope.dismissLoginModal = function () {
-        element.modal('hide');
-      };
-    }
-  };
-});
-
 var searchController = app.controller('SearchController',
-  ['$scope', '$http', '$httpParamSerializer',
-    function ($scope, $http, $httpParamSerializer) {
+    ['$scope', '$http', 'loginService',
+        function ($scope, $http, loginService) {
 
-      $scope.odataQuery = "mappe?$filter=contains(tittel, 'Oslo') $top=2$skip=4$orderby=tittel desc";
+            $scope.odataQuery = "mappe?$filter=contains(tittel, 'file')&$top=2&$orderby=tittel desc";
+            $scope.baseUrlForOdataSearch = "https://nikita.oslomet.no/noark5v5/api/arkivstruktur";
 
-      $scope.baseUrlForOdataSearch = GetBaseURLForODataSearch();
+            // It's not a secret
+            $scope.emailAddress = "admin@example.com";
+            $scope.password = "password";
+            doLoginSearch();
 
-      // Grab a copy of the authentication token
-      $scope.token = GetUserToken();
-      var loggedInUser = GetUsername();
-      if (loggedInUser !== null && loggedInUser !== undefined
-        && loggedInUser !== "") {
-        $scope.loggedIn = true;
-        $scope.emailAddress = loggedInUser;
-      }
-
-      // before a user tries to login.
-      $http({
-        method: 'GET',
-        url: baseUrl
-      }).then(function successCallback(response) {
-        for (var rel in response.data._links) {
-          var relation = response.data._links[rel].rel;
-          if (relation === REL_LOGIN_OAUTH2) {
-            $scope.loginHref = response.data._links[rel].href;
-            console.log("Setting login href to " + JSON.stringify($scope.loginHref));
-          }
-        }
-      });
-
-      function getJson() {
-        try {
-          if ($scope.odataResult != undefined)
-            return JSON.parse($scope.odataResult);
-          else
-            return JSON.parse("{}");
-        } catch (ex) {
-          alert('Wrong JSON Format: ' + ex);
-        }
-      }
-
-      var editor = new JsonEditor('#json-display', getJson());
-
-      $('#translate').on('click', function () {
-        editor.load(getJson());
-      });
-
-
-      $scope.doSearch = function () {
-
-        if ($scope.baseUrlForOdataSearch === "") {
-          console.log("$scope.baseUrlForOdataSearch is empty, don't know what to do!");
-          return;
-        }
-
-        // Need a better way to verify the text
-        if ($scope.odataQuery.length < 5) {
-          console.log("searchString.length < 5, don't know what to do!");
-          return;
-        }
-
-
-        var odataQuery = "mappe?$filter=contains(tittel, 'Oslo')&$top=2$skip=4$orderby=tittel desc";
-
-        document.write(odataQuery);
-        document.write("<br>");
-
-        const query = $scope.odataQuery.split("?");
-        const queryParameters = query[1].split("$");
-        let finalQuery = query[0] + "?";
-        for (let i = 0; i < queryParameters.length; i++) {
-          if (queryParameters[i] !== "") {
-            let parameterCommand = queryParameters[i].split("=");
-            finalQuery += "$" + parameterCommand[0] + "=" +
-                encodeURIComponent(parameterCommand[1]);
-            if (!parameterCommand[1].endsWith("&")) {
-              finalQuery += encodeURIComponent("&");
+            // Grab a copy of the authentication token
+            $scope.token = GetUserToken();
+            var loggedInUser = GetUsername();
+            if (loggedInUser !== null && loggedInUser !== undefined
+                && loggedInUser !== "") {
+                $scope.loggedIn = true;
+                $scope.emailAddress = loggedInUser;
             }
-          }
-        }
-        finalQuery = finalQuery.substring(0, finalQuery.length - 3);
-        var httpRequest = $scope.baseUrlForOdataSearch + "/" + finalQuery;
-        httpRequest = httpRequest.replace("api", "odata");
-        $http({
-          method: 'GET',
-          url: httpRequest,
-          headers: {'Authorization': $scope.token}
-        }).then(function successCallback(response) {
-          $scope.odataResult = JSON.stringify(response.data);
-          editor.load(getJson());
-        }, function (response) {
-          alert(JSON.stringify(response));
-        });
-      };
 
-      /**
-       * doLogin
-       *
-       * Perform the actual login and redirect the logged in user to the correct page
-       */
-      $scope.doLogin = function () {
-        console.log("Attempting to login using [" + $scope.loginHref + "]");
-        console.log("Username [" + $scope.emailAddress + "]");
-
-        $http({
-          url: $scope.loginHref,
-          method: "POST",
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded; charset=utf-8",
-            'Authorization': 'Basic ' + btoa('nikita-client:secret')
-          },
-          data: $httpParamSerializer({
-            grant_type: "password",
-            username: $scope.emailAddress,
-            password: $scope.password,
-            client_id: oauthClientId
-          })
-        }).then(function (data) {
-          SetUserToken("Bearer " + data.data.access_token);
-          console.log("Logging in.token is " + JSON.stringify(data.data));
-          $scope.token = "Bearer " + data.data.access_token;
-          console.log("Actual token is [" + $scope.token + "]");
-          SetUsername($scope.emailAddress);
-          $scope.loggedIn = true;
-          $scope.baseUrlForOdataSearch = "";
-          $http({
-            method: 'GET',
-            url: baseUrl,
-            headers: {'Authorization': $scope.token}
-          }).then(function successCallback(response) {
-            for (var rel in response.data._links) {
-              var relation = response.data._links[rel].rel;
-              //console.log ("REL [" + relation + "], " + REL_CHECK_TOKEN)
-              if (relation === REL_FONDS_STRUCTURE) {
-                $scope.baseUrlForOdataSearch = response.data._links[rel].href;
-                SetBaseURLForODataSearch($scope.baseUrlForOdataSearch);
-              }
+            function doLoginSearch() {
+                (async () => {
+                    try {
+                        $scope.tokenInfo = await loginService.doLogin(
+                            "https://nikita.oslomet.no/noark5v5/oauth/token",
+                            $scope.emailAddress, $scope.password);
+                        $scope.token = "Bearer " + $scope.tokenInfo[OAUTH_ACCESS_TOKEN];
+                        console.log("Token is " + $scope.token);
+                        SetUserToken("Bearer " + $scope.token);
+                        SetUserTokenInfo($scope.tokenInfo);
+                        SetUsername($scope.emailAddress);
+                        console.log("Logging in. Token is " + JSON.stringify(
+                            $scope.tokenInfo[OAUTH_ACCESS_TOKEN]));
+                        if ($scope.selectedLoginRole === ROLE_RECORDS_MANAGER) {
+                            changeLocation($scope, recordsManagerPage, true);
+                        } else if ($scope.selectedLoginRole === ROLE_CASE_HANDLER) {
+                            changeLocation($scope, caseHandlerPage, true);
+                        }
+                    } catch (error) {
+                        $scope.errorMessage = JSON.stringify(error.message);
+                        $scope.$apply($scope.error = true);
+                    }
+                })();
             }
-          });
-        });
-        $scope.dismissLoginModal();
-      };
 
-
-      $scope.doLogout = function () {
-        console.log("logoutAddress is " + $scope.hrefLogout);
-        var token = GetUserToken();
-        console.log("token is " + JSON.stringify(token));
-        if (typeof $scope.hrefLogout !== 'undefined') {
-          $http({
-            method: 'GET',
-            url: $scope.hrefLogout,
-            headers: {'Authorization': token}
-          }).then(function successCallback(response) {
-            $scope.projects = null;
-            console.log(" GET urlForLogout[" + $scope.hrefLogout +
-              "] returned " + JSON.stringify(response));
-            $scope.current = "";
-            $scope.emailAddress = "";
-            SetUsername("");
-            $scope.loggedIn = false;
-            $http({
-              method: 'GET',
-              url: baseUrl,
-            }).then(function successCallback(response) {
-
-              console.log("Application Root data is: " + JSON.stringify(response.data));
-
-              if (angular.isArray(response.data)) {
-                $scope.results = response.data;
-              } else {
-                $scope.results = [];
-                $scope.results.push(response.data);
-              }
-
-              $scope.parentHrefStack = [];
-              $scope.parentHrefStack.push(baseUrl);
-              $scope.parentHref = baseUrl;
-              $scope.selectedHref = baseUrl;
-              console.log("$scope.parentHref:" + $scope.parentHref);
-
-              for (var rel in response.data._links) {
-                var relation = response.data._links[rel].rel;
-                if (relation === REL_LOGIN_OAUTH2) {
-                  $scope.loginHref = response.data._links[rel].href;
-                  console.log("Setting login href to " + JSON.stringify($scope.loginHref));
+            function getJson() {
+                try {
+                    if ($scope.odataResult != undefined)
+                        return JSON.parse($scope.odataResult);
+                    else
+                        return JSON.parse("{}");
+                } catch (ex) {
+                    alert('Wrong JSON Format: ' + ex);
                 }
-              }
+            }
+
+            var editor = new JsonEditor('#json-display', getJson());
+
+            $('#translate').on('click', function () {
+                editor.load(getJson());
             });
 
 
-          }, function errorCallback(response) {
-            alert("Kunne ikke logge ut. " +
-              JSON.stringify(response));
-            console.log(" GET urlForLogout[" + $scope.hrefLogout +
-              "] returned " + JSON.stringify(response));
-          });
+            $scope.doSearch = function () {
+
+                if ($scope.baseUrlForOdataSearch === "") {
+                    console.log("$scope.baseUrlForOdataSearch is empty, don't know what to do!");
+                    return;
+                }
+
+                // Need a better way to verify the text
+                if ($scope.odataQuery.length < 5) {
+                    console.log("searchString.length < 5, don't know what to do!");
+                    return;
+                }
+                const query = $scope.odataQuery.split("?");
+                const queryParameters = query[1].split("$");
+                let finalQuery = query[0] + "?";
+                for (let i = 0; i < queryParameters.length; i++) {
+                    if (queryParameters[i] !== "") {
+                        let parameterCommand = queryParameters[i].split("=");
+                        finalQuery += "$" + parameterCommand[0] + "=" +
+                            encodeURIComponent(parameterCommand[1]);
+                        if (!parameterCommand[1].endsWith("&")) {
+                            finalQuery += encodeURIComponent("&");
+                        }
+                    }
+                }
+                finalQuery = finalQuery.substring(0, finalQuery.length - 3);
+                var httpRequest = $scope.baseUrlForOdataSearch + "/" + finalQuery;
+                $http({
+                    method: 'GET',
+                    url: httpRequest,
+                    headers: {'Authorization': $scope.token}
+                }).then(function successCallback(response) {
+                    $scope.odataResult = JSON.stringify(response.data);
+                    editor.load(getJson());
+                }, function (response) {
+                    alert(JSON.stringify(response));
+                });
+            };
         }
-      };
-    }
-  ])
+    ])
 ;
 
