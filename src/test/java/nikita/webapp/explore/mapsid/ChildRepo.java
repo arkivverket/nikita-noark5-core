dd1c57c6cad8db5135ce2b02bed28e71c8c00d98
Thomas Sødring <tsodring@gmail.com>
2021-03-22 10:51:04 +0100

    Fix wrong class

--- src/test/java/nikita/webapp/explore/mapsid/ChildRepo.java
+++ src/test/java/nikita/webapp/explore/mapsid/ChildRepo.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChildRepo
-        extends CrudRepository<Parent, String> {
+        extends CrudRepository<Child, String> {
 }
