a9da232b5f547ac5e6be421087125b9af6f93e3b
Thomas Sødring <tsodring@gmail.com>
2021-03-25 20:49:31 +0100

    Bring in class to help MockCustomUser

---
+++ src/test/java/nikita/webapp/structure/SpringSecurityWebAuxTestConfig.java
@@ -0,0 +1,26 @@
+package nikita.webapp.structure;
+
+import nikita.webapp.spring.TestSecurityConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Arrays;
+
+@TestConfiguration
+@Import(TestSecurityConfiguration.class)
+public class SpringSecurityWebAuxTestConfig {
+    @Bean
+    @Primary
+    public UserDetailsService userDetailsService() {
+        User user = new User("admin@example.com", "password",
+                Arrays.asList(
+                        new SimpleGrantedAuthority("RECORDS_MANAGER")));
+        return new InMemoryUserDetailsManager(Arrays.asList(user));
+    }
+}
