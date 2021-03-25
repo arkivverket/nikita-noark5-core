8c8c32ab036c7b3df782c4264a7e249e88bd85d5
Thomas Sødring <tsodring@gmail.com>
2021-03-25 18:18:04 +0100

    Update to match current inheritance structure
    We are moving to a JOINED approach for entities as hibernate does allow
    mixing strategies (joined / table per class). We are doing this as this makes
    sense for a number of reasons. 1 the ability to see a systemID in a systemID table, 2. the inheritance of File -> CaseFile becomes clearer

--- src/test/resources/db-tests/create_entity_fonds.sql
+++ src/test/resources/db-tests/create_entity_fonds.sql
@@ -1,9 +1,11 @@
-delete
-from as_fonds
-where 1;
-insert into as_fonds (owned_by, system_id, title, description, created_date, created_by)
-values ('admin@example.com', '3318a63f-11a7-4ec9-8bf1-4144b7f281cf', 'test title alpha', 'test description 1',
-        '2019-04-08 00:00:00', 'admin');
-insert into as_fonds (owned_by, system_id, title, description, created_date, created_by)
-values ('admin@example.com', 'f1677c47-99e1-42a7-bda2-b0bbc64841b7', 'test title bravo', 'test description 2',
-        '2019-04-08 00:00:00', 'admin');
+insert into system_id_entity(system_id, created_date, created_by, owned_by, version)
+values ('3318a63f-11a7-4ec9-8bf1-4144b7f281cf', '2019-04-08 00:00:00', 'admin@example.com', 'admin@example.com', 0);
+
+insert into system_id_entity(system_id, created_date, created_by, owned_by, version)
+values ('f1677c47-99e1-42a7-bda2-b0bbc64841b7', '2019-04-08 00:00:00', 'admin@example.com', 'admin@example.com', 0);
+
+insert into as_fonds (system_id, title, description)
+values ('3318a63f-11a7-4ec9-8bf1-4144b7f281cf', 'test title alpha', 'test description 1');
+
+insert into as_fonds (system_id, title, description)
+values ('f1677c47-99e1-42a7-bda2-b0bbc64841b7', 'test title alpha', 'test description 1');
