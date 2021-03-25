3dce89c9e1401693ed10db8ef0c11396da88be8f
Thomas Sødring <tsodring@gmail.com>
2021-03-25 19:48:20 +0100

    Make class a SystemID class
    Done to make things simpler and ensure consistent approach among similar classes e.g. PostalAddress

--- src/main/java/nikita/common/model/noark5/v5/casehandling/secondary/ResidingAddress.java
+++ src/main/java/nikita/common/model/noark5/v5/casehandling/secondary/ResidingAddress.java
@@ -1,73 +1,31 @@
 package nikita.common.model.noark5.v5.casehandling.secondary;
 
-import nikita.common.model.noark5.v5.NoarkEntity;
+import nikita.common.model.noark5.v5.SystemIdEntity;
 import nikita.common.model.noark5.v5.interfaces.entities.ISystemId;
 import nikita.common.model.noark5.v5.secondary.PartPerson;
-import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
-import java.util.UUID;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import static javax.persistence.FetchType.LAZY;
 import static nikita.common.config.Constants.TABLE_RESIDING_ADDRESS;
-import static nikita.common.config.N5ResourceMappings.SYSTEM_ID_ENG;
 
-/**
- * Created by tsodring on 5/14/17.
- */
 @Entity
 @Table(name = TABLE_RESIDING_ADDRESS)
 public class ResidingAddress
-        extends NoarkEntity
+        extends SystemIdEntity
         implements ISystemId {
-
-    /**
-     * M001 - systemID (xs:string)
-     */
-    @Id
-    @Type(type = "uuid-char")
-    @Column(name = SYSTEM_ID_ENG, insertable = false, updatable = false,
-            nullable = false)
-    private UUID systemId;
 
     @Embedded
     SimpleAddress simpleAddress;
 
     @OneToOne(fetch = LAZY)
-    @JoinColumn(name = SYSTEM_ID_ENG)
-    @MapsId
     CorrespondencePartPerson referenceCorrespondencePartPerson;
 
     @OneToOne(fetch = LAZY)
     private PartPerson partPerson;
-
-    @Override
-    public String getSystemId() {
-        if (null != systemId)
-            return systemId.toString();
-        else
-            return null;
-    }
-
-    @Override
-    public void setSystemId(UUID systemId) {
-        this.systemId = systemId;
-    }
-
-    @Override
-    public UUID getId() {
-        return systemId;
-    }
-
-    @Override
-    public void setId(UUID systemId) {
-        this.systemId = systemId;
-    }
-
-    @Override
-    public String getIdentifier() {
-        return getSystemId();
-    }
 
     public SimpleAddress getSimpleAddress() {
         return simpleAddress;
@@ -92,20 +50,5 @@ public class ResidingAddress
 
     public void setPartPerson(PartPerson partPerson) {
         this.partPerson = partPerson;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return super.equals(other);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
     }
 }
