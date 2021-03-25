c1f6e154848425604e682ffb21c3ea83ecb5e359
Thomas Sødring <tsodring@gmail.com>
2021-03-25 18:07:08 +0100

    Remove test.
    Test is now in StructureTest.java

--- src/test/java/nikita/webapp/explore/mapsid/TestMapsId.java
+++ src/test/java/nikita/webapp/explore/mapsid/TestMapsId.java
@@ -1,10 +1,5 @@
 package nikita.webapp.explore.mapsid;
 
-import nikita.common.model.noark5.v5.casehandling.secondary.BusinessAddress;
-import nikita.common.model.noark5.v5.casehandling.secondary.CorrespondencePartUnit;
-import nikita.common.model.noark5.v5.casehandling.secondary.PostalNumber;
-import nikita.common.model.noark5.v5.casehandling.secondary.SimpleAddress;
-import nikita.common.model.noark5.v5.metadata.CorrespondencePartType;
 import nikita.common.repository.n5v5.secondary.ICorrespondencePartRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,10 +11,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
 import java.util.UUID;
-
-import static nikita.common.config.MetadataConstants.CORRESPONDENCE_PART_CODE_EA;
-import static nikita.common.config.MetadataConstants.CORRESPONDENCE_PART_DESCRIPTION_EA;
-import static nikita.common.config.N5ResourceMappings.POSTAL_ADDRESS;
 
 /**
  * When working on the domain model, we are experiencing problems tidying
@@ -64,53 +55,5 @@ public class TestMapsId {
         Child persistedChild = entityManager
                 .find(Child.class, primaryKey);
         logger.info("Child object is " + persistedChild.toString());
-    }
-
-    @Test
-    /**
-     *
-     * A test that allows us to explore various configurations until we get
-     * it to work.
-     *
-     * See commit history to checkout various approaches if it is of interest
-     */
-    public void testCreateCorrespondBusiness() {
-        CorrespondencePartUnit correspondencePartUnit =
-                new CorrespondencePartUnit();
-
-        SimpleAddress simpleAddress = new SimpleAddress();
-        simpleAddress.setAddressLine1("Innsikten");
-        simpleAddress.setAddressLine2("Dronningensleilighet");
-        simpleAddress.setAddressLine3("Dronningssgate 19");
-        PostalNumber postalNumber = new PostalNumber();
-        postalNumber.setPostalNumber("5003");
-        simpleAddress.setPostalNumber(postalNumber);
-        simpleAddress.setPostalTown("Bergen");
-        simpleAddress.setAddressType(POSTAL_ADDRESS);
-
-        BusinessAddress businessAddress = new BusinessAddress();
-        businessAddress.setSimpleAddress(simpleAddress);
-        correspondencePartUnit.setName("Hans Gruber");
-
-        correspondencePartUnit.setBusinessAddress(businessAddress);
-        CorrespondencePartType correspondencePartType = new
-                CorrespondencePartType();
-        correspondencePartType.setCode(CORRESPONDENCE_PART_CODE_EA);
-        correspondencePartType.setCodeName(CORRESPONDENCE_PART_DESCRIPTION_EA);
-        correspondencePartUnit.setCorrespondencePartType(correspondencePartType);
-
-        correspondencePartRepository.save(correspondencePartUnit);
-        UUID primaryKey = UUID.fromString(correspondencePartUnit.getSystemId());
-
-        // Retrieve the object from the database to make sure everything is OK
-        CorrespondencePartUnit cpu = entityManager.find(
-                CorrespondencePartUnit.class, primaryKey);
-        logger.info("Parent object is " + cpu.toString());
-
-        // Make sure we can retrieve the related object using the same
-        // primary key
-        BusinessAddress ba = entityManager
-                .find(BusinessAddress.class, primaryKey);
-        logger.info("Child object is " + ba.toString());
     }
 }
