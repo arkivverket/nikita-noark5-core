df153e1597eb8a05a843dad87bf24b681136fd97
Thomas Sødring <tsodring@gmail.com>
2021-03-26 16:19:02 +0100

    Fix problem with JSON not being returned for OData parsing failure
    OData parsing is not being picked up by the normal mechanism in Nikita,
    which is likely due to the exception hierarchy. This fix picks up that
    exception and returns proper JSON, rather then simply returning string
    stating that an error occurred.

--- src/main/java/nikita/webapp/util/error/GlobalETAGExceptionHandler.java
+++ src/main/java/nikita/webapp/util/error/GlobalETAGExceptionHandler.java
@@ -1,37 +1,42 @@
 package nikita.webapp.util.error;
 
-import nikita.common.util.CommonUtils;
 import nikita.common.util.exceptions.NikitaETAGMalformedHeaderException;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static nikita.common.util.CommonUtils.WebUtils.getMethodsForRequestOrThrow;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-/**
- * Created by tsodring on 5/11/17.
- */
 @ControllerAdvice
 class GlobalETAGExceptionHandler {
 
     /**
-     * The point of this class is to capture exceptions that occur outside a controller.
+     * The point of this class is to capture exceptions that occur outside a
+     * controller.This is related to
+     * https://gitlab.com/OsloMet-ABI/nikita-noark5-core/issues/76
      * <p>
-     * This is related to https://gitlab.com/OsloMet-ABI/nikita-noark5-core/issues/76
+     * Also some antlr exceptions appear to go outside the
+     * ResponseEntityExceptionHandler approach, but this might be because of
+     * forwarding
      **/
-
-    @ExceptionHandler(value = NikitaETAGMalformedHeaderException.class)
-    public ResponseEntity<String> defaultErrorHandler(HttpServletRequest request, Exception ex) {
-        final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() : ex.getMessage();
-        final String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+    @ExceptionHandler(value = {NikitaETAGMalformedHeaderException.class,
+            Exception.class})
+    public ResponseEntity<ApiError> defaultErrorHandler(
+            HttpServletRequest request, Exception ex) {
+        final String message = ex.getMessage() == null ?
+                ex.getClass().getSimpleName() : ex.getMessage();
+        final String devMessage = getRootCauseMessage(ex);
         final String devStackTrace = ex.toString();
-
-        ApiError apiError = new ApiError(BAD_REQUEST, message, devMessage, devStackTrace);
+        ApiError apiError = new ApiError(
+                BAD_REQUEST, message.replace("\"", "\\\""),
+                devMessage.replace("\"", "\\\""),
+                devStackTrace.replace("\"", "\\\""));
         return ResponseEntity.status(BAD_REQUEST)
-                .allow(CommonUtils.WebUtils.getMethodsForRequestOrThrow(request.getServletPath()))
-                .body(apiError.toJSON());
+                .allow(getMethodsForRequestOrThrow(request.getServletPath()))
+                .body(apiError);
     }
 }
