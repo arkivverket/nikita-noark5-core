8e12465d15c3420ea704571c62f3f004c9110b8e
Thomas Sødring <tsodring@gmail.com>
2021-03-26 21:40:55 +0100

    Move test to correct location

--- src/test/java/nikita/webapp/spring/SpringSecurityWebAuxTestConfig.java
+++ src/test/java/nikita/webapp/spring/SpringSecurityWebAuxTestConfig.java
@@ -1,6 +1,5 @@
-package nikita.webapp.structure;
+package nikita.webapp.spring;
 
-import nikita.webapp.spring.TestSecurityConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
