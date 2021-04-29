aaf0662adaf5c2a781317b8afcc402f96dfa1f35
Thomas Sødring <tsodring@gmail.com>
2021-04-29 17:18:48 +0200

    Add missing annotations
    It's not possible to serialise a list of objects without them

--- src/main/java/nikita/common/model/noark5/v5/secondary/ScreeningMetadataLocal.java
+++ src/main/java/nikita/common/model/noark5/v5/secondary/ScreeningMetadataLocal.java
@@ -2,14 +2,18 @@ package nikita.common.model.noark5.v5.secondary;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import nikita.common.model.noark5.v5.NoarkEntity;
+import nikita.common.model.noark5.v5.SystemIdEntity;
+import nikita.common.model.noark5.v5.hateoas.secondary.ScreeningMetadataHateoas;
 import nikita.common.model.noark5.v5.interfaces.entities.IMetadataEntity;
+import nikita.common.model.noark5.v5.metadata.Metadata;
+import nikita.webapp.hateoas.secondary.ScreeningMetadataHateoasHandler;
+import nikita.webapp.util.annotation.HateoasObject;
+import nikita.webapp.util.annotation.HateoasPacker;
 import org.hibernate.envers.Audited;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
-import static javax.persistence.InheritanceType.SINGLE_TABLE;
 import static nikita.common.config.Constants.*;
 import static nikita.common.config.N5ResourceMappings.*;
 
@@ -27,130 +31,137 @@ import static nikita.common.config.N5ResourceMappings.*;
  */
 // Noark 5v5 skjermingmetadata
 @Entity
-@Inheritance(strategy = SINGLE_TABLE)
 @Table(name = TABLE_FONDS_STRUCTURE_SCREENING_METADATA)
+@HateoasPacker(using = ScreeningMetadataHateoasHandler.class)
+@HateoasObject(using = ScreeningMetadataHateoas.class)
 public class ScreeningMetadataLocal
-        extends NoarkEntity
+        extends SystemIdEntity
         implements IMetadataEntity {
 
-        /**
-         * M??? - kode (xs:string)
-         */
-        @Id
-        @Column(name = CODE_ENG)
-        @Audited
-        @JsonProperty(CODE)
-        protected String code;
+    /**
+     * M??? - kode (xs:string)
+     */
+    @Column(name = CODE_ENG)
+    @Audited
+    @JsonProperty(CODE)
+    protected String code;
+    /**
+     * M??? - kodenavn (xs:string)
+     */
+    @Column(name = CODE_NAME_ENG)
+    @Audited
+    @JsonProperty(CODE_NAME)
+    protected String codeName;
 
-        /**
-         * M??? - kodenavn (xs:string)
-         */
-        @Column(name = CODE_NAME_ENG)
-        @Audited
-        @JsonProperty(CODE_NAME)
-        protected String codeName;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = SCREENING_SCREENING_METADATA_ID,
+            referencedColumnName = PRIMARY_KEY_SYSTEM_ID)
+    @JsonIgnore
+    private Screening referenceScreening;
 
-        @ManyToOne(fetch = LAZY)
-        @JoinColumn(name = SCREENING_SCREENING_METADATA_ID,
-                referencedColumnName = PRIMARY_KEY_SYSTEM_ID)
-        @JsonIgnore
-        private Screening referenceScreening;
+    public ScreeningMetadataLocal() {
+    }
 
-        @Override
-        public String getCode() {
-                return code;
-        }
+    public ScreeningMetadataLocal(Metadata metadata) {
+        this.code = metadata.getCode();
+        this.codeName = metadata.getCodeName();
+    }
 
-        @Override
-        public void setCode(String code) {
-                this.code = code;
-        }
+    @Override
+    public String getCode() {
+        return code;
+    }
 
-        @Override
-        public String getCodeName() {
-                return codeName;
-        }
+    @Override
+    public void setCode(String code) {
+        this.code = code;
+    }
 
-        @Override
-        public void setCodeName(String codeName) {
-                this.codeName = codeName;
-        }
+    @Override
+    public String getCodeName() {
+        return codeName;
+    }
 
-        /**
-         * This method will always return the randomly chosen value false as
-         * it only implements IMetadataEntity so that we can reuse the
-         * serialisation code for Metadata entities. This class is in a
-         * unique position that it is in a 0:m relationship with Screening so
-         * it cannot be implemented the same as the other Metadata classes
-         *
-         * @return null
-         */
-        @Override
-        public Boolean getInactive() {
-                return false;
-        }
+    @Override
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
 
-        /**
-         * This method will always ignore the value sent in from calling it as
-         * it only implements IMetadataEntity so that we can reuse the
-         * serialisation code for Metadata entities. This class is in a
-         * unique position that it is in a 0:m relationship with Screening so
-         * it cannot be implemented the same as the other Metadata classes
-         */
-        @Override
-        public void setInactive(Boolean inactive) {
+    /**
+     * This method will always return the randomly chosen value false as
+     * it only implements IMetadataEntity so that we can reuse the
+     * serialisation code for Metadata entities. This class is in a
+     * unique position that it is in a 0:m relationship with Screening so
+     * it cannot be implemented the same as the other Metadata classes
+     *
+     * @return null
+     */
+    @Override
+    public Boolean getInactive() {
+        return false;
+    }
 
-        }
+    /**
+     * This method will always ignore the value sent in from calling it as
+     * it only implements IMetadataEntity so that we can reuse the
+     * serialisation code for Metadata entities. This class is in a
+     * unique position that it is in a 0:m relationship with Screening so
+     * it cannot be implemented the same as the other Metadata classes
+     */
+    @Override
+    public void setInactive(Boolean inactive) {
 
-        public Screening getReferenceScreening() {
-                return referenceScreening;
-        }
+    }
 
-        public void setReferenceScreening(Screening referenceScreening) {
-                this.referenceScreening = referenceScreening;
-        }
+    public Screening getReferenceScreening() {
+        return referenceScreening;
+    }
 
-        @Override
-        public String getBaseTypeName() {
-                return SCREENING_METADATA;
-        }
+    public void setReferenceScreening(Screening referenceScreening) {
+        this.referenceScreening = referenceScreening;
+    }
 
-        /**
-         * This method will always ignore the value sent in from calling it as
-         * it only implements IMetadataEntity so that we can reuse the
-         * serialisation code for Metadata entities. This class is in a
-         * unique position that it is in a 0:m relationship with Screening so
-         * it cannot be implemented the same as the other Metadata classes
-         */
-        @Override
-        public void setBaseTypeName(String baseTypeName) {
-        }
+    @Override
+    public String getBaseTypeName() {
+        return SCREENING_METADATA;
+    }
 
-        /**
-         * This method will always ignore the value sent in from calling it as
-         * it only implements IMetadataEntity so that we can reuse the
-         * serialisation code for Metadata entities. This class is in a
-         * unique position that it is in a 0:m relationship with Screening so
-         * it cannot be implemented the same as the other Metadata classes
-         */
-        @Override
-        public String setFunctionalTypeName(String functionalTypeName) {
-                return null;
-        }
+    /**
+     * This method will always ignore the value sent in from calling it as
+     * it only implements IMetadataEntity so that we can reuse the
+     * serialisation code for Metadata entities. This class is in a
+     * unique position that it is in a 0:m relationship with Screening so
+     * it cannot be implemented the same as the other Metadata classes
+     */
+    @Override
+    public void setBaseTypeName(String baseTypeName) {
+    }
 
-        @Override
-        public String getBaseRel() {
-                return REL_FONDS_STRUCTURE_SCREENING_METADATA;
-        }
+    /**
+     * This method will always ignore the value sent in from calling it as
+     * it only implements IMetadataEntity so that we can reuse the
+     * serialisation code for Metadata entities. This class is in a
+     * unique position that it is in a 0:m relationship with Screening so
+     * it cannot be implemented the same as the other Metadata classes
+     */
+    @Override
+    public String setFunctionalTypeName(String functionalTypeName) {
+        return null;
+    }
 
-        /**
-         * This method will always ignore the value sent in from calling it as
-         * it only implements IMetadataEntity so that we can reuse the
-         * serialisation code for Metadata entities. This class is in a
-         * unique position that it is in a 0:m relationship with Screening so
-         * it cannot be implemented the same as the other Metadata classes
-         */
-        @Override
-        public void setBaseRel(String baseRel) {
-        }
+    @Override
+    public String getBaseRel() {
+        return REL_FONDS_STRUCTURE_SCREENING_METADATA;
+    }
+
+    /**
+     * This method will always ignore the value sent in from calling it as
+     * it only implements IMetadataEntity so that we can reuse the
+     * serialisation code for Metadata entities. This class is in a
+     * unique position that it is in a 0:m relationship with Screening so
+     * it cannot be implemented the same as the other Metadata classes
+     */
+    @Override
+    public void setBaseRel(String baseRel) {
+    }
 }
