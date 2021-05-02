50667dc05920b3da78fb339b76fc581cb8d70c9f
Thomas Sødring <tsodring@gmail.com>
2021-05-02 07:38:09 +0200

    Make constants static

--- src/main/java/nikita/common/util/deserialisers/ClassDeserializer.java
+++ src/main/java/nikita/common/util/deserialisers/ClassDeserializer.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 import static nikita.common.config.HATEOASConstants.LINKS;
+import static nikita.common.config.N5ResourceMappings.CLASS_ID;
 import static nikita.common.util.CommonUtils.Hateoas.Deserialize.*;
 
 /**
