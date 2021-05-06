5601cef19248a56587ebe0fdb31a15a8a0600fcf
Thomas Sødring <tsodring@gmail.com>
2021-05-06 22:50:27 +0200

    Add delete tests
    runtest is reporting problems with delete o documentflow and precedence. These test address this.

--- src/test/java/nikita/webapp/general/DeleteTest.java
+++ src/test/java/nikita/webapp/general/DeleteTest.java
@@ -52,4 +52,44 @@ public class DeleteTest
         resultActions.andExpect(status().isNoContent());
         printDocumentation(resultActions);
     }
+
+    /**
+     * Check that it is possible to delete a DocumentFlow associated with a
+     * RegistryEntry
+     *
+     * @throws Exception Serialising or validation exception
+     */
+    @Test
+    @Sql({"/db-tests/basic_structure.sql", "/db-tests/document_flow.sql"})
+    @WithMockCustomUser
+    public void deleteDocumentFlowWithRegistryEntry() throws Exception {
+        String urlRegistryEntry = "/noark5v5/api/sakarkiv/" +
+                "journalpost/8f6b084f-d727-4b46-bbe2-14bed2135fa9/";
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .delete(urlRegistryEntry)
+                .contextPath(contextPath)
+                .accept(NOARK5_V5_CONTENT_TYPE_JSON));
+        resultActions.andExpect(status().isNoContent());
+        printDocumentation(resultActions);
+    }
+
+    /**
+     * Check that it is possible to delete a DocumentFlow associated with a
+     * RecordNote
+     *
+     * @throws Exception Serialising or validation exception
+     */
+    @Test
+    @Sql({"/db-tests/basic_structure.sql", "/db-tests/document_flow.sql"})
+    @WithMockCustomUser
+    public void deleteDocumentFlowWithRecordNote() throws Exception {
+        String urlRecordNote = "/noark5v5/api/sakarkiv/" +
+                "arkivnotat/11b32a9e-802d-43de-9bb5-c951e3bbe95b";
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .delete(urlRecordNote)
+                .contextPath(contextPath)
+                .accept(NOARK5_V5_CONTENT_TYPE_JSON));
+        resultActions.andExpect(status().isNoContent());
+        printDocumentation(resultActions);
+    }
 }
