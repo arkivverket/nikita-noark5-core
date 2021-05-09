ec4c89f6cde944096020425fc3d0c1fa833df97a
Thomas Sødring <tsodring@gmail.com>
2021-05-09 07:59:28 +0200

    Fix unparameterised warning

--- src/main/java/nikita/common/util/deserialisers/casehandling/CaseFileDeserializer.java
+++ src/main/java/nikita/common/util/deserialisers/casehandling/CaseFileDeserializer.java
@@ -26,7 +26,7 @@ import static nikita.common.util.CommonUtils.Hateoas.Deserialize.*;
  * - Missing obligatory property values in the JSON will trigger an exception
  */
 public class CaseFileDeserializer
-        extends JsonDeserializer {
+        extends JsonDeserializer<CaseFile> {
 
     private static final Logger logger =
             LoggerFactory.getLogger(CaseFileDeserializer.class);
