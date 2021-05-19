bd99d2462d755d3ef39d4c75c02df92488112303
Thomas Sødring <tsodring@gmail.com>
2021-05-19 11:15:56 +0200

    Make File support pagination
    File was chosen to be the first class to use pagination as it is the most important class to require it as users will typically pull multiple files when interacting with the API

--- src/main/java/nikita/common/model/noark5/v5/hateoas/FileHateoas.java
+++ src/main/java/nikita/common/model/noark5/v5/hateoas/FileHateoas.java
@@ -1,27 +1,31 @@
 package nikita.common.model.noark5.v5.hateoas;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import nikita.common.config.N5ResourceMappings;
+import nikita.common.model.nikita.NikitaPage;
 import nikita.common.model.noark5.v5.interfaces.entities.INoarkEntity;
 import nikita.common.util.serializers.noark5v5.hateoas.FileHateoasSerializer;
 
 import java.util.List;
 
+import static nikita.common.config.N5ResourceMappings.FILE;
+
 /**
- * Created by tsodring on 12/9/16.
- * <p>
- * Using composition rather than inheritance. Although this class is really only a placeholder for the File object
- * along with the hateoas links. It's not intended that you will manipulate the file object from here.
+ * add File specific Hateoas links
  */
 @JsonSerialize(using = FileHateoasSerializer.class)
-public class FileHateoas extends HateoasNoarkObject implements IHateoasNoarkObject {
+public class FileHateoas
+        extends HateoasNoarkObject
+        implements IHateoasNoarkObject {
 
     public FileHateoas(INoarkEntity entity) {
         super(entity);
     }
 
     public FileHateoas(List<INoarkEntity> entityList) {
-        super(entityList, N5ResourceMappings.FILE);
+        super(entityList, FILE);
     }
 
+    public FileHateoas(NikitaPage page) {
+        super(page, FILE);
+    }
 }
