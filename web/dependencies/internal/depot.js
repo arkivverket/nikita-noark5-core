60a91c6787e99d09c44edacdb847ba5561235742
Ole Aamot <ole@gnome.org>
2021-08-18 19:45:28 +0200

    New Archive Depot Service in web/depot.html with internal/depot.js

---
+++ web/dependencies/internal/depot.js
@@ -0,0 +1,153 @@
+/**
+ * DepotController
+ *
+ * This file provides the following functionality:
+ *
+ *  1. When the page loads the nikita core is called to retrieve the login URL
+ *    - In this case, we look for a OAUTH2 REL (https://nikita.arkivlab.no/noark5/v5/login/rfc6749)
+ *  2. When a successful depot occurs, the user is pushed to the correct html page
+ *   - This process will also issue a GET to the root of the application and
+ *     retrieve and GET the content behind https://rel.arkivverket.no/noark5/v5/api/arkivstruktur/.
+ *     This object is then stored in localstorage so the application knows how to retrieve
+ *     fonds objects and create them.
+ *
+ */
+
+var depot = app.controller('DepotController',
+    ['$scope', '$http', 'depotService',
+        function ($scope, $http, depotService) {
+
+            // Grab a copy of the authentication token
+            $scope.token = GetUserToken();
+            $scope.oidc = GetOIDCInfo();
+
+            if ($scope.oidc === undefined) {
+                console.log("Missing OIDC information. Missing token functionality");
+            }
+
+            $scope.errorMessage = MSG_NIKITA_DOWN;
+            $scope.error = false;
+
+            //$scope.emailAddress = "admin@example.com";
+            //$scope.password = "password";
+
+            // This sets 'arkivar' to be the default choice on the webpage
+            $scope.selectedDepotRole = "arkivar";
+            $scope.selectedDepotRole = "saksbehandler";
+
+            // Set values for dropdown in webpage
+            $scope.depotOptions = depotOptions;
+
+            // index.html, booleans to display cards
+            $scope.showDepot = true;
+            $scope.showAbout = false;
+            $scope.showSourceCode = false;
+
+            // Set account details blank
+            $scope.emailAddressRegister = "";
+            $scope.passwordRegister = "";
+            $scope.repeatPasswordRegister = "";
+            $scope.firstnameRegister = "";
+            $scope.lastnameRegister = "";
+
+            (async () => {
+                try {
+                    $scope.oidc = await depotService.getDepotHrefViaOIDC();
+                    SetOIDCInfo($scope.oidc);
+                } catch (error) {
+                    $scope.$apply($scope.error = true);
+                }
+            })();
+
+            /**
+             * doDepot
+             *
+             * Perform the actual depot and redirect the logged in user to the correct page
+             *
+             */
+            $scope.doDepot = function () {
+                (async () => {
+                    try {
+                        $scope.tokenInfo = await depotService.doDepot($scope.oidc[DEPOT_ENDPOINT],
+                            $scope.emailAddress, $scope.password);
+                        $scope.token = $scope.tokenInfo[OAUTH_ACCESS_TOKEN];
+                        console.log("Token is " + $scope.token);
+                        SetUserToken("Bearer " + $scope.token);
+                        SetUserTokenInfo($scope.tokenInfo);
+                        SetUsername($scope.emailAddress);
+                        console.log("Logging in. Token is " + JSON.stringify(
+                            $scope.tokenInfo[OAUTH_ACCESS_TOKEN]) +
+                            ". Redirecting to page for " + $scope.selectedDepotRole);
+                        if ($scope.selectedDepotRole === ROLE_RECORDS_MANAGER) {
+                            changeLocation($scope, recordsManagerPage, true);
+                        } else if ($scope.selectedDepotRole === ROLE_LEADER) {
+                            changeLocation($scope, leaderPage, true);
+                        } else if ($scope.selectedDepotRole === ROLE_CASE_HANDLER) {
+                            changeLocation($scope, caseHandlerPage, true);
+                        } else if ($scope.selectedDepotRole === ROLE_POST_MOTTAK) {
+                            changeLocation($scope, postmottakPage, true);
+                        } else if ($scope.selectedDepotRole === ROLE_QUALITY_CONTROL) {
+                            changeLocation($scope, qualityPage, true);
+                        }
+                    } catch (error) {
+                        $scope.errorMessage = JSON.stringify(error.message);
+                        $scope.$apply($scope.error = true);
+                    }
+                })();
+            };
+
+            /**
+             * createAccount
+             *
+             * Create an account for nikita
+             *
+             */
+            $scope.createUser = function () {
+                let url = $scope.oidc[REGISTRATION];
+                console.log("Attempting to create User using [" + url + "]");
+                if ($scope.repeatPasswordRegister === $scope.passwordRegister) {
+                    $http({
+                        url: url,
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Basic ' + btoa('nikita-client:secret')
+                        },
+                        data: {
+                            brukerNavn: $scope.emailAddressRegister,
+                            passord: $scope.passwordRegister,
+                            fornavn: $scope.firstnameRegister,
+                            etternavn: $scope.lastnameRegister
+                        }
+                    }).then(function (data) {
+                        console.log("Created new user. " + JSON.stringify(data.data));
+                        $scope.dismissNewUserModal();
+                        $scope.emailAddress = $scope.emailAddressRegister;
+                    }, function (response) {
+                        $scope.dismissNewUserModal();
+                        console.log(JSON.stringify(response));
+                    });
+                } else {
+                    $scope.passwordNotMatching = true;
+                    console.log("NO MATCH");
+                }
+            };
+
+            $scope.showDepotCard = function () {
+                $scope.showDepot = true;
+                $scope.showAbout = false;
+                $scope.showSourceCode = false;
+            };
+
+            $scope.showAboutCard = function () {
+                $scope.showDepot = false;
+                $scope.showAbout = true;
+                $scope.showSourceCode = false;
+            };
+
+            $scope.showSourceCodeCard = function () {
+                $scope.showDepot = false;
+                $scope.showAbout = false;
+                $scope.showSourceCode = true;
+            };
+        }]);
